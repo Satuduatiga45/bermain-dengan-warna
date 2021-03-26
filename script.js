610234f3b450cbc442fca258d17e92b46aca96c7
Me <satuduatiga4554321@gmail.com>
2021-03-26 11:57:38 +0700

    Commit awal

---
+++ script.js
@@ -0,0 +1,10 @@
+// input[type="range"]
+const r = document.getElementById('red');
+const g = document.getElementById('green');
+const b = document.getElementById('blue');
+
+// div
+const redDiv = document.querySelector('.red');
+const greenDiv = document.querySelector('.green');
+const blueDiv = document.querySelector('.blue');
+
